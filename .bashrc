5d062542624d48c1cf830e452761ea51a32bd167
blentar <dilan@arch>
2022-03-23 18:45:55 -0400

    paleofetch

--- .bashrc
+++ .bashrc
@@ -5,7 +5,9 @@
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
-fortune | cowthink
+#fortune | cowthink
+
+paleofetch
 
 alias config="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 alias cl='clear'
@@ -22,6 +24,8 @@ alias nightly='firefox-nightly'
 alias ls='lsd --color=auto'
 alias swayrc='vim ~/.config/sway/config'
 alias swaylock='swaylock -i ~/git/dracula/wallpaper/base.png'
+alias cf='vim ~/.bashrc'
+alias fetch='paleofetch'
 
 # PS1='[\u@h \W]\$ '
 
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,22 +1,23 @@
 function fish_greeting
-    cowthink (fortune)
+    paleofetch
 end
 
 alias config="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 alias cl='clear'
 alias nf='neofetch'
-alias ls='lsd'
 alias cat='bat'
 alias cf='vim ~/.config/fish/config.fish'
 alias po='poweroff'
 alias sd='shutdown'
 alias rb='reboot'
 alias x='exit'
-# wayland
 alias copy='wl-copy'
 alias paste='wl-paste'
-
 alias nightly='firefox-nightly'
+alias ls='lsd --color=auto'
+alias swayrc='vim ~/.config/sway/config'
+alias swaylock='swaylock -i ~/git/dracula/wallpaper/base.png'
+alias cf='vim ~/.bashrc'
+alias fetch='paleofetch'
 
 starship init fish | source
-
