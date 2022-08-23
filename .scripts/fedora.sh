cb0dbfdcc778f64c3212d511067cfa0904f25ca3
blentar <dilan@sus>
2022-08-23 16:20:22 -0400

    jetbrainsnf

--- .scripts/fedora.sh
+++ .scripts/fedora.sh
@@ -11,7 +11,7 @@ echo "HISTFILE=~/.cache/bash/history" >> .bashrc
 [ -f "$HOME/.bash_history" ] && mv ~/.bash_history ~/.cache/bash/history
 
 sudo dnf update
-sudo dnf install util-linux-user yaru-theme alacritty zsh neovim wl-clipboard mozilla-fira-sans-fonts papirus-icon-theme mozilla-fira-fonts-common fira-code-fonts gnome-tweaks
+sudo dnf install gh util-linux-user papirus-icon-theme alacritty zsh neovim wl-clipboard mozilla-fira-sans-fonts papirus-icon-theme gnome-tweaks
 
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf groupupdate core
@@ -21,7 +21,7 @@ sudo dnf groupupdate sound-and-video
 flatpak remote-delete flathub
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-flatpak install  extensionmanager polymc
+flatpak install extensionmanager polymc
 
 sudo dnf copr enable shrisha/gnome-shell-mutter-tripplebuffered-41
 sudo dnf update
@@ -29,10 +29,10 @@ sudo dnf update
 sudo dnf copr enable nickavem/adw-gtk3
 sudo dnf install adw-gtk3
 
-mkdir -p ~/gitstuff/zsh-plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/gitstuff/zsh-plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/gitstuff/zsh-plugins/zsh-syntax-highlighting
-git clone https://github.com/marlonrichert/zsh-autocomplete ~/gitstuff/zsh-plugins/zsh-autocomplete
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip 
+unzip ~/Downloads/JetBrainsMono.zip -d ~/Downloads/JetBrainsMono/
+sudo cp ~/Downloads/JetBrainsMono/* /usr/share/fonts/
+fc-cache -fv
 
 echo ".dotfiles" >> .gitignore
 git clone --bare https://github.com/blentar/dotfiles .dotfiles
