c998d105db9cae4e1693c1cff4dea5524ccd6809
blentar <dilan@dilan>
2023-02-17 07:27:55 -0500

    random

--- .config/nvim/lua/blentar/settings.lua
+++ .config/nvim/lua/blentar/settings.lua
@@ -10,4 +10,7 @@ vim.o.laststatus = 2
 vim.o.autowrite = true
 vim.o.cursorline = true
 vim.o.autoread = true
-vim.api.nvim_set_option("clipboard","unammed")
+
+vim.o.clipboard = 'unnamedplus'
+--vim.api.nvim_buf_set_option(0, ':set', 'clipboard+=unnamedplus')
+-- set clipboard+=unnamedplus
